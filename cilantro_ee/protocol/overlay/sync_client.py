7f64e38c006e55d27de8c995e59d5e8aaef19ce0
raghupc <raghupc@hotmail.com>
2019-10-03 16:43:58 -0700

    fixes after testing

--- cilantro_ee/protocol/overlay/sync_client.py
+++ cilantro_ee/protocol/overlay/sync_client.py
@@ -1,4 +1,5 @@
 from cilantro_ee.protocol.overlay.client import OverlayClient
+from math import ceil
 
 import time
 import asyncio
@@ -6,11 +7,15 @@ import asyncio
 class OverlayClientSync(OverlayClient):
     def __init__(self, ctx):
         super().__init__(self._handle_overlay_reply, self._handle_overlay_reply, ctx=ctx)
+        self.timeout = 5         # secs
+        self.sleep_time = 0.2    # secs
+     
+        self.timeout_iter = ceil(self.timeout / self.sleep_time)
 
         self.events = {}
 
     def _handle_overlay_reply(self, e):
-        self.log.debugv("OverlayClientSync got overlay reply {}".format(e))
+        self.log.debugv("raghu OverlayClientSync got overlay reply {}".format(e))
 
         if 'event_id' in e:
             self.events[e['event_id']] = e
@@ -19,20 +24,13 @@ class OverlayClientSync(OverlayClient):
             self.log.debugv("OverlayClientSync got overlay response {} that has no event_id associated. "
                             "Ignoring.".format(e['event']))
 
-    def get_ip_sync(self, vk):
-        event_id = self.get_ip_from_vk(vk)
-        while event_id not in self.events:
-            time.sleep(0.2)
-        e = self.events[event_id]
-        if e['event'] == 'got_ip':
-            return e['ip']
-        return None
-
     async def async_get_ip_sync(self, vk):
         event_id = self.get_ip_from_vk(vk)
-        while event_id not in self.events:
-            await asyncio.sleep(0.2)
-        e = self.events[event_id]
-        if e['event'] == 'got_ip':
+        iter = 0
+        while (iter < self.timeout_iter) and (event_id not in self.events):
+            await asyncio.sleep(self.sleep_time)
+            iter += 1
+        e = self.events.pop(event_id, None)
+        if e and e['event'] == 'got_ip':
             return e['ip']
         return None
